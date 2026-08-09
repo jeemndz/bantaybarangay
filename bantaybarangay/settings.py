42fe3f4e0f728c604345d2b187e9f360d0ca7fe8
jiem <johnlurjmendoza1@gmail.com>
2026-08-09 17:08:26 +0800

    Co-authored-by: AirwinMaui <AirwinMaui@users.noreply.github.com> Co-authored-by: Amiel26 <Amiel26@users.noreply.github.com>

--- bantaybarangay/settings.py
+++ bantaybarangay/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'bantaybarangay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add this line to specify the templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- residentmodule/views.py
+++ residentmodule/views.py
@@ -3,10 +3,8 @@ from .models import Resident
 
 # Create your views here.
 def resident_list(request):
-    residents = Resident.objects.all()
+    residents = Resident.objects.all() # for returning all residents from the database
 
     context = {'residents': residents}
 
-    return render(request, 'resident_list.html', context)
-
-
+    return render(request, 'resident_list.html', context) # for rendering the resident_list.html template with the residents data
